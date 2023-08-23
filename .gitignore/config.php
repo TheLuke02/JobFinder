d29fa0a6b1274707617bba5c1e0d0d3da45b8822
Luca La Spina <lucalaspina2002@gmail.com>
2023-08-23 17:06:04 +0200

    change DB server name

--- .gitignore/config.php
+++ .gitignore/config.php
@@ -2,7 +2,7 @@
   $host = "localhost";
   $db_user = "zoihmnjx_root";
   $db_psw = "asdAD73/asd.";
-  $db_name = "zoihmnjx_wp578";
+  $db_name = "zoihmnjx_J  obFinder";
 
   $cnn = "mysql:host=$host;dbname=$db_name";
 ?>
