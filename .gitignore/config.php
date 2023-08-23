5b15a18e32f6bc294da85434c4aecc81bcf72ec0
Luca La Spina <lucalaspina2002@gmail.com>
2023-08-23 16:57:57 +0200

    Changing the config file

--- .gitignore/config.php
+++ .gitignore/config.php
@@ -1,7 +1,7 @@
 <?php
   $host = "localhost";
   $db_user = "zoihmnjx_root";
-  $db_psw = "w@H#wSe9yxMt5E";
+  $db_psw = "asdAD73/asd.";
   $db_name = "zoihmnjx_wp578";
 
   $cnn = "mysql:host=$host;dbname=$db_name";
